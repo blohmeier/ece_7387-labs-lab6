083c8d9cdf908e0d862b1be23abdb7c3cc989ef5
Brett Lohmeier <brett.lohmeier@gmail.com>
2019-11-06 15:09:40 -0600

    Update c17.v
    before shortening to try and instantiate "Dff_.v" file

--- try2/c17.v
+++ try2/c17.v
@@ -1,10 +1,5 @@
 `timescale 1ns / 1ps
-// Verilog
-// c17
-// Ninputs 5
-// Noutputs 2
-// NtotalGates 6
-// NAND2 6
+
 module c17(N1,N2,N3,N6,N7,N22,N23,clk);
 
 input N1,N2,N3,N6,N7,clk;
@@ -12,7 +7,29 @@ input N1,N2,N3,N6,N7,clk;
 output N22,N23;
 
 wire N10,N11,N16,N19;
-reg NAND2_1q;
+
+nand NAND2_1 (N10, N1, N3);
+nand NAND2_2 (N11, N3, N6);
+nand NAND2_3 (N16, N2, N11);
+nand NAND2_4 (N19, N11, N7);
+nand NAND2_5 (N22, N10, N16);
+nand NAND2_6 (N23, N16, N19);
+
+
+// Single stage pipeline (at inputs and outputs)
+reg N1NAND2_1,N2NAND2_3,N3NAND2_1,N3NAND2_2,N6NAND2_2,N7NAND2_4;
+reg NAND2_5N22,NAND2_5N23;
+
+always @ (posedge clk) begin
+    N1NAND2_1 <= N1;    //  dff on N1 input to NAND2_1
+    N3NAND2_1 <= N3;    //  dff on N3 input to NAND2_1
+    N3NAND2_2 <= N3;    //  dff on N3 input to NAND2_2
+    N6NAND2_2 <= N6;    //  dff on N6 input to NAND2_2
+    N2NAND2_3 <= N2;    //  dff on N2 input to NAND2_3
+    N7NAND2_4 <= N7;    //  dff on N7 input to NAND2_4
+end
+
+/* Two pipeline stages (additionally at wires N10 and N11)
 
 nand NAND2_1 (N10, N1, N3);
 nand NAND2_2 (N11, N3, N6);
@@ -21,8 +38,9 @@ nand NAND2_4 (N19, N11, N7);
 nand NAND2_5 (N22, NAND2_1q, N16);
 nand NAND2_6 (N23, N16, N19);
 
+// Single stage pipeline (at inputs and outputs)
 always @ (posedge clk) begin
     NAND2_1q <= N10; //dff on N10 wire
-end
+end */
 
 endmodule
