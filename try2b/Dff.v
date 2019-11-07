917eb0655e009c02662467f6dd2fb1dddbcee410
Brett Lohmeier <brett.lohmeier@gmail.com>
2019-11-07 07:48:56 -0600

    Update Dff.v
    updated to show dff with async set / reset

--- try2b/Dff.v
+++ try2b/Dff.v
@@ -6,13 +6,10 @@ module Dff_(
 
 reg q;
 
-always @(posedge clk) begin
-    if(clk == 1) 
-        begin
-            if (r == 1) q = 0;  
-            else if(s == 1) q = 1; 
-        else if (sync == 1) q = d;    
-        end      
+always @(posedge clk or posedge r or posedge s or posedge sync) begin
+    if(r == 1) q = 0;
+    else if(s == 1) q = 1; 
+    else if (clk == 1 && sync == 1) q = d;
 end
 
 endmodule
